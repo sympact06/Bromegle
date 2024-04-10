d03891a8f3d3f03acdd63aba6d02b723ae0a4be7
Olivier <47295195+sympact06@users.noreply.github.com>
2024-04-10 14:33:59 +0200

    Add Tailwind CSS to app.css
    This commit includes the initial setup for Tailwind CSS in the application. The base, components, and utilities styles are imported into the app.css file for global usage.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,3 +13,11 @@ window.Echo = new Echo({
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     encrypted: true
 });
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
